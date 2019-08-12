fe703876071e7e8268a945298a6caf35a7c65dba
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-08-12 15:42:48 +0530

    Update module.ec2.tf

--- layers/DEV/module.ec2.tf
+++ layers/DEV/module.ec2.tf
@@ -13,4 +13,14 @@ module "ec2" {
  module "kms" {
    source = "../../modules/kms"
   }
- 
+ module "alb" {
+  source = "../../modules/alb"
+  name_prefix            = "${var.name_prefix}"
+  image_id               = "${var.image_id}"
+  instance_type          = "${var.instance_type}"
+  key_name               = "${var.key_name}"
+  availability_zones     = "${var.availability_zones}"
+  subnets                = "${var.subnets}"
+  certificate_arn        = "${var.certificate_arn}"
+  ssl_policy             = "${var.ssl_policy}"
+}

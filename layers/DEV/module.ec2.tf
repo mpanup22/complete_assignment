3e16ce42ca0c6c091eee42e8dcd88a92c2c59520
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-08-12 17:22:06 +0530

    Update module.ec2.tf

--- layers/DEV/module.ec2.tf
+++ layers/DEV/module.ec2.tf
@@ -19,7 +19,7 @@ module "ec2" {
   image_id               = "${var.image_id}"
   instance_type          = "${var.instance_type}"
   key_name               = "${var.key_name}"
-  availability_zones     = "${var.availability_zone}"
+  availability_zones     = "${var.availability_zones}"
   subnets                = "${var.subnets}"
   certificate_arn        = "${var.certificate_arn}"
   ssl_policy             = "${var.ssl_policy}"

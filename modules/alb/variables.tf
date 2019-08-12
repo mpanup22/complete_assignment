fa3e740d2c2fe1cf5bc927d1c12b591f7a4f24f8
mpanup22 <47411805+mpanup22@users.noreply.github.com>
2019-08-12 17:51:41 +0530

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -36,6 +36,10 @@ description ="name of launch configuration"
 variable "image_id" {
   description ="ami to attach autoscaling group"
 }
+variable "environment_tag" {
+  description = "defining the environment"
+  default= "DEV_EC2"
+}
 
 # APPLICATION LOAD BALANCER PARAMETERS
 variable "subnets" {
